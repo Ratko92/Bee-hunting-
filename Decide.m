e91a9666a66b4f25e75b346ae315e3d9c739b28a
Ratko Pilipovic <ratko.pilipovic@etfbl.net>
2016-10-13 14:26:46 +0200

    Decide.m, NMC doesn't consider L* component

--- Decide.m
+++ Decide.m
@@ -2,8 +2,10 @@ function [ y ] = Decide( HB_desc, centroid_pollen, centroid_without_pollen )
     
    pollen_diff = HB_desc - centroid_pollen;
    without_pollen_diff = HB_desc - centroid_without_pollen;
-   
-   if (sqrt(pollen_diff * pollen_diff')) < (sqrt(without_pollen_diff * without_pollen_diff')) 
+   tmp1 = [pollen_diff(1), pollen_diff(3:4)];
+   tmp2 = [without_pollen_diff(1), without_pollen_diff(3:4)];
+
+   if (sqrt(tmp1* tmp1')) < (sqrt(tmp2 * tmp2')) 
         y = 1;
    else 
         y = 0;
